19741dc20879eb02dcf7b2b7b93b6efca05eff79
Auxi <auxilpge@gmail.com>
2021-08-19 08:46:02 +0200

    Flechas para slider grande con flechas ajustadas de slider horizontal

--- js/apps.js
+++ js/apps.js
@@ -1,21 +1,63 @@
+//Menú de navegación
 const Fabars = document.querySelector('.fa-bars')
 const Nav = document.querySelector('nav')
 const Arrow = document.querySelector('.arrow')
 
-// Primer slider con 1 comentario
+// Carrusel de comentarios (solo 1)
 const lines = document.querySelectorAll('.slider_line')
 const container = document.querySelector('.container')
 const slider = document.querySelector('.slider')
 const next = document.querySelector('.slider_next')
 const prev = document.querySelector('.slider_prev')
 
-//Prueba de slider 2
-const prueba       = document.querySelector('.prueba')
-const botones      = document.querySelectorAll('.prueba__button') 
+//Carrusel de comentarios (3)
+const slider_2       = document.querySelector('.slider_2')
+const botones      = document.querySelectorAll('.slider_2_button') 
 const [ next2 , prev2 ] = botones
 const grande       = document.querySelector('.grande')
 const puntos__li   = document.querySelectorAll('.puntos__li')    
 const paneles      = document.querySelectorAll('.grande__panel')
+
+// Carrusel vertical
+const section = document.querySelectorAll('.slider__section')
+const grande = document.querySelector('.slider__grande')
+
+
+//Flechas para bajar las secciones de la primera pantalla
+let lineaActiva = 0
+
+
+document.body.addEventListener('keydown', function(e){
+    console.log(e.key)
+
+    if( e.key =='ArrowUp' || e.key =='ArrowDown'){
+          let nextLinea = lineaActiva + 1
+          if (nextLinea >= linea.length)
+          {
+                nextLinea = 0
+          }
+          moveToElement(nextLinea)
+    }
+})
+function moveToElement(i)
+{
+      grande.style.transform = 'translateY(-' + (i * 50) + '%)'
+      
+            
+      linea.forEach(function(cadaLinea, i){
+            linea[i].classList.remove('activo')
+      })
+      linea[i].classList.add('activo')
+
+      section.forEach(function(cadaSection, i){
+            section[i].classList.remove('activo')
+      })
+      section[i].classList.add('activo')
+
+      lineaActiva = i
+}
+
+
 
 
 //Menú de navegación cambiar de jq a js
@@ -47,6 +89,7 @@ function desplazar(){
     })
     lines[foto].classList.add('active')
 }
+
 function siguiente (){
     foto++
     if (  foto > 3){
@@ -54,6 +97,7 @@ function siguiente (){
     }
     desplazar()
 }
+
 function anterior(){
     foto--
     if ( foto < 0 ){
@@ -65,16 +109,14 @@ function anterior(){
 // Prueba 2
 let   foto2         = 0
 let   anchoPanel   = 100 / paneles.length
-// let   automatico   = setInterval( siguiente , 2000)
+
 puntos__li.forEach( function( cadaPunto , i ){
     puntos__li[i].addEventListener('click',function(){
         foto2 = i
         desplazar2()
     })
 })
-
 next2.addEventListener('click', siguiente2 )
-
 prev2.addEventListener('click', anterior2)
 
 //automático
@@ -86,8 +128,6 @@ prev2.addEventListener('click', anterior2)
 // prueba.addEventListener('mouseout',function(){
 //     automatico   = setInterval( siguiente2 , 2000)
 // })
-
-
 function desplazar2(){
     grande.style.transform = 'translateX(-' + foto2 * anchoPanel + '%)'
     puntos__li.forEach( function( cadaPunto , i ){
